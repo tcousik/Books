387a5ce8c5e6c02690010dc4a10edf62720962d2
tcousik <contejas1@gmail.com>
2024-07-14 21:03:35 -0400

    react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import ReadBook from './pages/ReadBook'
+import UpdateBook from './pages/UpdateBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ReadBook />} />
+      <Route path='/books/update/:id' element={<UpdateBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
