18cbd6c4b3a1e32ef768a1f831525467598410e1
tcousik <contejas1@gmail.com>
2024-07-16 22:42:40 -0400

    made card component

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { MdOutlineDelete } from 'react-icons/md';
 
 const BooksTable = ({ books }) => {
     return (
